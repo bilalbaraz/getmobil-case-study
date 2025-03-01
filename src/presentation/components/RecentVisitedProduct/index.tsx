dd0f96f1942ed40993b7684f5cde4eacb99f9a05
Bilal Baraz <bilalbaraz@windowslive.com>
2025-03-01 11:32:03 +0300

    Refactor RecentVisitedProduct component

--- src/presentation/components/RecentVisitedProduct/index.tsx
+++ src/presentation/components/RecentVisitedProduct/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Alert, Dimensions, TouchableOpacity, View } from 'react-native';
+import { Alert, TouchableOpacity, View } from 'react-native';
 import styles from './styles';
 import FastImage from 'react-native-fast-image';
 import { Text } from 'react-native-paper';
-import { COLORS } from '@constants/colors';
 
 const RecentVisitedProduct = ({ product }: any) => {
     return (
@@ -11,11 +10,11 @@ const RecentVisitedProduct = ({ product }: any) => {
             <View style={styles.container}>
                 <FastImage
                     source={{ uri: product.image }} 
-                    style={{width: '100%', height: Dimensions.get('window').width * 0.2}}
+                    style={styles.image}
                     resizeMode={FastImage.resizeMode.cover}
                 />
-                <View style={{position: 'absolute', bottom: 0, left: 0, right: 0, backgroundColor: 'rgba(0, 0, 0, 0.5)', padding: 5}}>
-                    <Text style={{color: COLORS.white, fontSize: 12, fontWeight: '500'}} numberOfLines={1}>{product.name}</Text>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.titleText} numberOfLines={1}>{product.name}</Text>
                 </View>
             </View>
         </TouchableOpacity>
--- src/presentation/components/RecentVisitedProduct/styles.tsx
+++ src/presentation/components/RecentVisitedProduct/styles.tsx
@@ -3,7 +3,7 @@ import { Dimensions, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
-        width: Dimensions.get('window').width * 0.2,
+        width: Dimensions.get('window').width * 0.25,
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'center',
@@ -11,8 +11,27 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: COLORS.border,
         backgroundColor: COLORS.white,
-        padding: 5,
+        padding: 0,
+        borderRadius: 5,
+        overflow: 'hidden',
         marginLeft: 5
+    },
+    image: {
+        width: '100%', 
+        height: Dimensions.get('window').width * 0.25
+    },
+    titleContainer: {
+        position: 'absolute', 
+        bottom: 0, 
+        left: 0, 
+        right: 0, 
+        backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+        padding: 5
+    },
+    titleText: {
+        color: COLORS.white, 
+        fontSize: 12, 
+        fontWeight: '500'
     },
     flex1: {flex: 1},
 });
